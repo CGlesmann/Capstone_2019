bc85e97c049fb6651de8b915618bcd90bcb1c00e
Matthew Campbell <mcjc4397@gmail.com>
2019-04-08 08:32:22 -0700

    fix up coding
    I fix up the player combat coding and made it were the arm and staff pointing where you are looking.

---
+++ Capstone_2k19/Assets/Scripts/PlayerCombatController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatController : MonoBehaviour
+{
+    public Rigidbody fireBallPrefab;
+    public Transform magicHand;
+    public float fireBallSpeed = 500;
+    public float fireRate = 60;
+    private float timer = 0;
+
+    void Update()
+    {
+        if (timer == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Rigidbody fireBallInstance;
+                fireBallInstance = Instantiate(fireBallPrefab, magicHand.position, magicHand.rotation) as Rigidbody;
+                fireBallInstance.AddForce(magicHand.forward * fireBallSpeed);
+                timer = fireRate;
+            }
+        }
+        else
+        {
+            timer -= 1;
+        }
+    }
+}
