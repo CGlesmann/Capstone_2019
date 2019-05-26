d66ef5bee8041226be4b62e2d67b15928f111bc2
LightAxe2402 <chrisglesmann@gmail.com>
2019-05-25 18:56:25 -0700

    Option Menu/Pause Menu/Nav Mesh Data

---
+++ Capstone_2k19/Assets/Scripts/OptionsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionsManager : MonoBehaviour
+{
+    [Header("Option References")]
+    [SerializeField] private AudioMixerGroup musicMixer = null;
+    [SerializeField] private AudioMixerGroup sfxMixer = null;
+
+    [Header("GUI References")]
+    [SerializeField] private Slider musicSlider = null;
+    [SerializeField] private Slider sfxSlider = null;
+
+    private void Awake()
+    {
+        // Music Volume
+        musicMixer.audioMixer.GetFloat("Volume", out float mVal);
+        musicSlider.value = mVal;
+
+        // SFX Volume
+        sfxMixer.audioMixer.GetFloat("Volume", out float sVal);
+        sfxSlider.value = sVal;
+    }
+
+    public void SetMusicVolume() { musicMixer.audioMixer.SetFloat("Volume", musicSlider.value); }
+    public void SetSfxVolume() { sfxMixer.audioMixer.SetFloat("Volume", sfxSlider.value); }
+
+    public void ExitOptions(string sceneName) { SceneManager.LoadScene(sceneName); }
+}
---
+++ Capstone_2k19/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    // Singleton reference
+    public static PauseManager manager = null;
+
+    [Header("Pause Variables")]
+    public bool isPaused = false;
+    public GameObject pauseMenu = null;
+
+    /// <summary>
+    /// Sets the singleton pattern
+    /// </summary>
+    private void Awake()
+    {
+        if (manager == null)
+        {
+            manager = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+        else
+            GameObject.Destroy(gameObject);
+
+        // Setting the Cursor Lock
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    /// <summary>
+    /// Check for Enabling the Pause Menu
+    /// </summary>
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+                PauseGame();
+            else
+                UnPauseGame();
+        }
+    }
+
+    /// <summary>
+    /// Pause Toggles
+    /// </summary>
+    public void PauseGame() { pauseMenu.SetActive(true); isPaused = true; Cursor.lockState = CursorLockMode.None; Cursor.visible = true; }
+    public void UnPauseGame() { pauseMenu.SetActive(false); isPaused = false; Cursor.lockState = CursorLockMode.Locked; Cursor.visible = false; }
+
+    public void ExitGame() { Application.Quit(); }
+}
