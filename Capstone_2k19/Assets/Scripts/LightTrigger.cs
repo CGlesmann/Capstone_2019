4c7798dfba951173e18fdbbca20e475201b57416
LightAxe2402 <chrisglesmann@gmail.com>
2019-05-30 19:53:24 -0700

    Torch Trigger added.

---
+++ Capstone_2k19/Assets/Scripts/LightTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class LightTrigger : MonoBehaviour
+{
+    [Header("Torch Effect References")]
+    [SerializeField] private GameObject lightSource = null;
+    [SerializeField] private GameObject fireEffect = null;
+
+    /// <summary>
+    /// Sets the torch to be off by default
+    /// </summary>
+    private void Awake()
+    {
+        lightSource.SetActive(false);
+        fireEffect.SetActive(false);
+    }
+
+    /// <summary>
+    /// Activate the light and particle effect
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.gameObject;
+
+        if (obj.CompareTag("Player"))
+        {
+            lightSource.SetActive(true);
+            fireEffect.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// De-activates the light and particle effect
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject obj = other.gameObject;
+
+        if (obj.CompareTag("Player"))
+        {
+            lightSource.SetActive(false);
+            fireEffect.SetActive(false);
+        }
+    }
+}
