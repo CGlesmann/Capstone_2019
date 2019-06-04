9f4255e44411ac67cbc4ac2966f1f798a15ad391
Matt4397 <mcjc4397@gmail.com>
2019-06-04 14:00:34 -0700

    bug fixes and finished new stuff.

--- Capstone_2k19/Assets/Scripts/Player/MeleeHitBox.cs
+++ Capstone_2k19/Assets/Scripts/Player/MeleeHitBox.cs
@@ -3,20 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeHitBox : MonoBehaviour
-{    
-    GameObject Player = GameObject.Find("Player");
+{
+    public float setcollectingTime = 30;
+    private float collectingTime = 0;
 
     public void OnTriggerEnter(Collider col)
     {
         BoxCollider hitbox = gameObject.GetComponent<BoxCollider>();
 
-        if (col.gameObject.tag == "Enemy")
+        if (col.gameObject.tag == "Enemy" && gameObject.GetComponent<PlayerCombatController>().attacking == true)
         {                    
             col.gameObject.GetComponent<CombatCharacter>().TakeDamage(20);
             col.gameObject.GetComponent<CombatCharacter>().meleeAttacked = true;
 
             Debug.Log("Enemy Entered");
             hitbox.enabled = false;
+        }
+
+        if (col.gameObject.name.Contains("DeadEnemy"))
+        {
+            Debug.Log("Collection time set");
+            collectingTime = setcollectingTime;  
+        }
+    }
+
+    public void OnTriggerStay(Collider col)
+    {
+        GameObject Player = GameObject.Find("Player");
+
+        if (col.gameObject.name.Contains("DeadEnemy"))
+        {
+            if (collectingTime > 0)
+            {
+                Debug.Log("Collecting Mana");
+                collectingTime -= 1;
+            }
+            else if(collectingTime <= 0)
+            {
+                Player.gameObject.GetComponent<CombatCharacter>().RestoreMana(30);
+                Destroy(col.gameObject);
+            }
         }
     }
 }
